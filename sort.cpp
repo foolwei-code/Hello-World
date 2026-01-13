4ff44cfa806b6bd7356d9d2f05ade55989149ca1
WSL_wrg <123456wsl@qq.com>
2026-01-13 19:33:00 +0800

    排序部分已经完成，并生成了可执行程序

--- sort.cpp
+++ sort.cpp
@@ -2,6 +2,13 @@
 #include<vector>
 using namespace std;
 void bubble_sort(vector<int>&vec)
+{
+    int n=vec.size();
+    for(int i=n-1;i>=0;i--)
+      for(int j=0;j<i;j++)
+	 if(vec[j]>vec[j+1])
+		 swap(vec[j],vec[j+1]);
+}
 int main()
 {
     vector<int>vec{5,4,3,2,1};
